f6615c71ceda05edea54a19f32e9f5b9803e05fd
Md Fojailibne Nur Akhanda <mdfojaelakhonda@gmail.com>
2025-12-22 12:57:51 +0600

    initial commit

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('category/<slug:val>', views.CategoryView.as_view(), name='category'),
+    path('product-detail/<int:pk>', views.ProductDetail.as_view(), name='product-detail'),
 ]
